7258ce0ab9105a1123fcb91e9e9ff44c639f1bcc
Noah A Klem <noahaklem@gmail.com>
2021-11-10 16:06:40 -0700

    commit tree 492040cd2feec581d5dc02d47650c702c6b70e65

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`);
+};
+
+const mondayWork = function(activity = 'go to the office') {
+    return (`This Monday, I will ${activity}.`)
+};
+
+const wrapAdjective = function(flare = '*') {
+    return function(adjective = "special") {
+        return (`You are ${flare}${adjective}${flare}!`)
+    }
+};
+
+const Calculator = {
+    add: function(a, b){
+        return a + b
+    },
+    subtract: function(a, b) {
+        return a - b
+    },
+    multiply: function(a, b) {
+        return a * b
+    },
+    divide: function(a, b) {
+        return a / b
+    }
+};
+
+let actionApplyer = function(start, ray) {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+    return a
+};
